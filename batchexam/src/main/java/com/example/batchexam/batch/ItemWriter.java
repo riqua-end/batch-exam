2ff073d173dc7a2baf44927f67c992909db84ba5
Kim KJ <riqua1@naver.com>
2024-05-16 21:34:15 +0900

    디렉토리 수정

--- batchexam/src/main/java/com/example/batchexam/batch/ItemWriter.java
+++
@@ -1,6 +0,0 @@
-package com.example.batchexam.batch;
-
-public interface ItemWriter <O>{
-
-    void write(O item);
-}
--- batchexam/src/main/java/com/example/batchexam/customer/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.batchexam.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
