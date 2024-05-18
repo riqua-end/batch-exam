01b90f00e19cc06ab8521ea0503e52ee3ca43750
Kim KJ <riqua1@naver.com>
2024-05-18 17:39:59 +0900

    ** Spring Batch Example **
    1. Step - flow 실습

---
+++ ch02 . batchexam/service/src/main/java/com/example/batchexam/FlowConfiguration.java
@@ -0,0 +1,68 @@
+package com.example.batchexam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class FlowConfiguration {
+
+    @Bean
+    public Job job (
+            JobRepository jobRepository,
+            Step step1,
+            Step step2,
+            Step step3
+    ) {
+        return new JobBuilder("flowJob", jobRepository)
+                .start(step1)
+//                    .on("*").to(step2)
+//                .from(step1)
+//                    .on("FAILED").to(step3) // step1 에서 실패시 step3
+//                    .on("FAILED").end() // job execution 에는 completed , step execution 에는 failed
+//                .on("FAILED").fail() // job , step execution failed
+                .on("COMPLETED").stopAndRestart(step2) // job execution status stop
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((a,b)->{
+                    log.info("step1 실행");
+                    /*if (1==1) {
+                        throw new IllegalStateException("step1 실패");
+                    }*/
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((a,b)->{
+                    log.info("step2 실행");
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((a,b)->{
+                    log.info("step3 실행");
+                    return null;
+                }, platformTransactionManager)
+                .build();
+    }
+}
--- ch02 . batchexam/service/src/main/java/com/example/batchexam/JobConfiguration.java
+++ ch02 . batchexam/service/src/main/java/com/example/batchexam/JobConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-@Configuration
+//@Configuration
 @Slf4j
 public class JobConfiguration {
 
