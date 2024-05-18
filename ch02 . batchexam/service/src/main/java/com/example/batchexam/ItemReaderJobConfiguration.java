38b38e29f9329b676e332ee3b9ee6fffdd3e0d12
Kim KJ <riqua1@naver.com>
2024-05-18 18:33:50 +0900

    ** Spring Batch Example **
    1. FlatFileItemReader 와 JsonItemReader 실습

---
+++ ch02 . batchexam/service/src/main/java/com/example/batchexam/ItemReaderJobConfiguration.java
@@ -0,0 +1,83 @@
+package com.example.batchexam;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class ItemReaderJobConfiguration {
+
+    @Bean
+    public Job job (
+            JobRepository jobRepository,
+            Step step1
+    ) {
+        return new JobBuilder("itemReaderJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public Step step (
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager,
+            ItemReader<User> jsonItemReader
+    ) {
+        return new StepBuilder("step", jobRepository)
+                .<User, User>chunk(2,platformTransactionManager)
+                .reader(jsonItemReader)
+                .writer(System.out::println)
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> flatFileItemReader() {
+        return new FlatFileItemReaderBuilder<User>()
+                .name("flatFileItemReader")
+                .resource(new ClassPathResource("users.txt"))
+                .linesToSkip(2)
+                .delimited().delimiter("|") // 구분자 default "," .delimiter() 를 안써도 됨
+                .names("name","age","region","telephone")
+                .targetType(User.class)
+                .strict(false) // false 는 해당 file 이 없어도 정상적으로 실행, true 는 해당 file 이 없으면 IllegalStateException 발생함
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> fixedLengthFlatFileItemReader(){
+        return new FlatFileItemReaderBuilder<User>()
+                .name("fixedLengthFlatFileItemReader")
+                .resource(new ClassPathResource("usersFixedLength.txt"))
+                .linesToSkip(2)
+                .fixedLength()
+                .columns(new Range[]{new Range(1,2), new Range(3,4), new Range(5,6), new Range(7,19)})
+                .names("name","age","region","telephone")
+                .targetType(User.class)
+                .strict(false)
+                .build();
+    }
+
+    @Bean
+    public JsonItemReader<User> jsonItemReader() {
+        return new JsonItemReaderBuilder<User>()
+                .name("jsonItemReader")
+                .resource(new ClassPathResource("users.json"))
+                .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
+                .build();
+    }
+}
---
+++ ch02 . batchexam/service/src/main/java/com/example/batchexam/User.java
@@ -0,0 +1,12 @@
+package com.example.batchexam;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private String age;
+    private String region;
+    private String telephone;
+}
