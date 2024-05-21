1032a44285f89e01421f24554f294ab83c1c8d44
Kim KJ <riqua1@naver.com>
2024-05-21 17:16:55 +0900

    ** Spring Batch Example **
    1. ItemProcessor 실습
    
    ItemProcessor 로 비즈니스 로직을 실행시키거나 변경하거나 필요없으면 통과시키거나 관심사가 많아지면 관심사를 분리하여 실행시킬수있음

---
+++ ch02 . batchexam/service/src/main/java/com/example/batchexam/ItemProcessorJobConfiguration.java
@@ -0,0 +1,96 @@
+package com.example.batchexam;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class ItemProcessorJobConfiguration {
+
+    @Bean
+    public Job job (
+            JobRepository jobRepository,
+            Step step
+    ) {
+        return new JobBuilder("itemWriterJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step (
+            JobRepository jobRepository,
+            PlatformTransactionManager platformTransactionManager,
+            ItemReader<User> flatFileItemReader
+    ) {
+        final List<ItemProcessor<User, User>> list = Arrays.asList(processor1(), processor2(), processor3());
+
+        return new StepBuilder("step", jobRepository)
+                .<User, User>chunk(2, platformTransactionManager)
+                .reader(flatFileItemReader)
+                .processor(new CompositeItemProcessor<>(list))
+                .writer(System.out::println)
+                .build();
+    }
+
+    private static ItemProcessor<User, String> customProcessor() { // 필터링도 가능
+        return user -> {
+            if (user.getName().equals("민수")) return null;
+
+            return "%s의 나이는 %s 입니다. 사는곳은 %s, 전화번호는 %s 입니다.".formatted(
+                    user.getName(), user.getAge(), user.getRegion(), user.getTelephone()
+            );
+        };
+    }
+
+    private static ItemProcessor<User, User> processor1() {
+        return user -> {
+            user.setName(user.getName() + user.getName());
+            return user;
+        };
+    }
+
+    private static ItemProcessor<User, User> processor2() {
+        return user -> {
+            user.setAge(user.getAge() + user.getAge());
+            return user;
+        };
+    }
+
+    private static ItemProcessor<User, User> processor3() {
+        return user -> {
+            user.setRegion(user.getRegion() + user.getRegion());
+            return user;
+        };
+    }
+
+    @Bean
+    public FlatFileItemReader<User> flatFileItemReader() {
+        return new FlatFileItemReaderBuilder<User>()
+                .name("flatFileItemReader")
+                .resource(new ClassPathResource("users.txt"))
+                .linesToSkip(2)
+                .delimited().delimiter(",") // 구분자 default "," .delimiter() 를 안써도 됨
+                .names("name","age","region","telephone")
+                .targetType(User.class)
+                .strict(true) // false 는 해당 file 이 없어도 정상적으로 실행, true 는 해당 file 이 없으면 IllegalStateException 발생함
+                .build();
+    }
+}
